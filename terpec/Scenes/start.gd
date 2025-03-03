dace138b8f9ee9c2482a155f4a70c40caa632a9a
MistMaser <mistmaser@gmail.com>
2025-03-03 19:45:34 +0500

    03032025

--- terpec/Scenes/start.gd
+++ terpec/Scenes/start.gd
@@ -1,8 +1,8 @@
 extends Node
 @onready var animated_sprite_2d = $AnimatedSprite2D
-var next_scene = preload("res://Scenes/main_menu.tscn")
 
 func _ready():
+	preload("res://Scenes/main_menu.tscn")
 	animated_sprite_2d.play("start")
 	await $AnimatedSprite2D.animation_finished
-	get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
